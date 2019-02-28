ecaf2a34bd2bae9d7b4d160ece9051cd24b30894
Marta Niemiec <niemiec.marta@hotmail.com>
2019-02-28 15:43:20 +0100

    added face recognition and box with a recognized face

--- src/App.js
+++ src/App.js
@@ -30,8 +30,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
 
   // EVENT LISTENER onInputChange (pass it as a prop into ImageLinkForm here inside this class and into ImageLinkForm.js)
@@ -43,18 +62,11 @@ class App extends Component {
     this.setState({imageUrl: this.state.input}) 
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
-      this.state.input) {/* setState() in React is asynchronous, so here we CAN'T put this.state.imageUrl;
-      One way to go around this issue is to use a callback function: 
-      setState(updater, callback)*/}
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        // console.log(this.state.input);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      this.state.input) // setState() in React is asynchronous, so here we CAN'T put this.state.imageUrl;
+      // One way to go around this issue is to use a callback function: 
+      // setState(updater, callback)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -71,7 +83,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         /> 
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
