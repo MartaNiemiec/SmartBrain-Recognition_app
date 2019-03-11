7c0129f0fe4814d10333f5932e3f924519239e00
Marta Niemiec <niemiec.marta@hotmail.com>
2019-03-11 13:06:52 +0100

    added /profile:id and /image

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -15,7 +15,7 @@ const database = {
       joined: new Date()
     },
     {
-      id: '1234',
+      id: '124',
       name: 'Sally',
       email: 'sally@gmail.com',
       password: 'bananas',
@@ -25,9 +25,11 @@ const database = {
   ]
 }
 
+
 app.get('/', (req, res) => {
   res.send(database.users);
 })
+
 
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && 
@@ -39,6 +41,7 @@ app.post('/signin', (req, res) => {
   // res.send('signing')
   res.json('signing')
 })
+
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
@@ -52,6 +55,38 @@ app.post('/register', (req, res) => {
   })
   res.json(database.users[database.users.length-1]);
 })
+
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400),json('not found');
+  }
+})
+
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400),json('not found');
+  }
+})
+
 
 app.listen(3000, () => {
   console.log('app is runnng on port 3000');
